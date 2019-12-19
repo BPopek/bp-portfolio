40afdb65246347c6f73b8226e5990bf7e6a16d91
Betsy Popek <betsypopek@gmail.com>
2019-12-19 15:34:23 -0700

    added pdf and graphicDesignBA

---
+++ src/Graphic-Design-Folder/GraphicDesignBA.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+
+//images for CPO carousel
+import BA1 from '../images/GD/shirts/Blue Angels/BA671111.jpg';
+import BA2 from '../images/GD/shirts/Blue Angels/BA671112.jpg';
+import BA3 from '../images/GD/shirts/Blue Angels/FFE2354_BY.jpg';
+import BA4 from '../images/GD/shirts/Blue Angels/FFE2355_BY.jpg';
+import BA5 from '../images/GD/shirts/Blue Angels/FFE2634_BY.jpg';
+import BA6 from '../images/GD/shirts/Blue Angels/FFE2355_BY.jpg';
+
+class GraphicDesignBA extends Component {
+
+    render(){
+        return(
+            <div className='parentDiv'>
+                <Switch>
+                    <Route path='/graphic-design/BlueAngels'>
+                        <p className='projectDescriptiononSlideshow'>
+                            Xxxxxxxx.<br />
+                        </p>
+                    <div className='carouselDiv'>
+                            <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} swipeable={true} dynamicHeight={true} useKeyboardArrows={true}  >
+                                <div className='imageCarousel'>
+                                    <img src={BA1} alt='Blue Angels Hoodie Design'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={BA2} alt='Blue Angels Shirt Design'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={BA3} alt='Blue Angels Cap Design'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={BA4} alt='Blue Angels Cap Design'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={BA5} alt='Blue Angels Cap Design'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={BA6} alt='Blue Angels Cap Design'/>
+                                </div>
+                            </Carousel>
+                        </div>               
+                    </Route>    
+                </Switch>
+            </div>
+        )
+    }
+}
+
+export default GraphicDesignBA;
---
+++ src/Graphic-Design-Folder/GraphicDesignMisc.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import { Carousel } from 'react-responsive-carousel';
+
+//images for BC carousel
+import misc1 from '../images/GD/MISC/IMG_4730.jpg'
+import misc2 from '../images/GD/MISC/IMG_4829.jpg'
+import misc3 from '../images/GD/MISC/IMG_4830.jpg'
+
+class GraphicDesignMisc extends Component {
+
+    render(){
+        
+        return(
+            <div className='parentDiv'>
+                <Switch>
+                    <Route path='/graphic-design/MISC'>
+                        <p className='projectDescriptiononSlideshow'>
+                            xxxxxx.<br />
+                        </p>
+                    <div className='carouselDiv'>
+                            <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} swipeable={true} dynamicHeight={true} useKeyboardArrows={true}  >
+                                <div className='imageCarousel'>
+                                    <img src={misc1} alt='Boy Scout Jamboree Belt Buckle'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={misc2} alt='Boy Scout SBR Bolo Tie'/>
+                                </div>
+                                <div className='imageCarousel'>
+                                    <img src={misc3} alt='Boy Scout SBR Bolo Tie'/>
+                                </div>
+                            </Carousel>
+                        </div>   
+                    </Route>                 
+                </Switch>
+            </div>
+        )
+    }
+}
+
+export default GraphicDesignMisc;
