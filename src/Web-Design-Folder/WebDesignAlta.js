28ba2047635721642ae7ba45e86da35c2499a314
Betsy Popek <betsypopek@gmail.com>
2019-12-20 10:09:20 -0700

    https

--- src/Web-Design-Folder/WebDesignAlta.js
+++ src/Web-Design-Folder/WebDesignAlta.js
@@ -7,14 +7,14 @@ class WebDesignAlta extends Component {
         return(
             <div className='parentDiv'>
                 <div className='webGitHubLinks'>
-                    <a href="http://business-time-alta.surge.sh/" className='projectLinkWebPage'>Visit Alta Ski Area Project Website</a> 
+                    <a href="https://business-time-alta.surge.sh/" className='projectLinkWebPage'>Visit Alta Ski Area Project Website</a> 
                     <br></br>
                     <a href="https://github.com/BPopek/Business-Time---Alta-Ski-Area" className='projectLinkWebPage'> Visit Github Repository</a>
                 </div>
                 <p className='projectDescriptionWeb'>
                     Exercise to create a business website for Alta Ski Area.<br /> Website is fully responsive, utilizing three different sizes to cleanly organize information.<br /> Website is built using CSS & HTML.
                 </p>
-                <Iframe url='http://business-time-alta.surge.sh/'
+                <Iframe url='https://business-time-alta.surge.sh/'
                     className='iframe'
                     display='initial'/>
                 <p className='iframeDisclaimer'>Note: iFrame viewer shown above does not display responsive content. <br /> Window itself is responsive and will resize, but content is not responsive.</p>
--- src/Web-Design-Folder/WebDesignCH.js
+++ src/Web-Design-Folder/WebDesignCH.js
@@ -8,14 +8,14 @@ class WebDesignCH extends Component {
         return(
             <div className='parentDiv'>
                 <div className='webGitHubLinks'>
-                    <a href="http://cooper-hewitt-sdm.surge.sh/" className='projectLinkWebPage'>Visit Cooper Hewitt Museum Project Website</a> 
+                    <a href="https://cooper-hewitt-sdm.surge.sh/" className='projectLinkWebPage'>Visit Cooper Hewitt Museum Project Website</a> 
                     <br></br>
                     <a href="https://github.com/BPopek/Cooper-Hewitt-SDM" className='projectLinkWebPage'>Visit Github Repository</a>
                 </div>
                 <p className='projectDescriptionWeb'>
                     React website project utilizing the Cooper Hewitt Museum API to provide an interactive experience for viewing museum experience.<br /> Fully responsive and utilizes multiple API endpoints provided by Cooper Hewitt Museum.<br />Developed front-end design and functionality for a clean and informative experience.<br />Built with React.js, JavaScript, HTML, CSS, Axios, and Node.js.
                 </p>
-                    <Iframe url='http://cooper-hewitt-sdm.surge.sh/'
+                    <Iframe url='https://cooper-hewitt-sdm.surge.sh/'
                         className='iframe'
                         display='initial'/>
                     <p className='iframeDisclaimer'>Note: iFrame viewer shown above does not display responsive content. <br /> Window itself is responsive and will resize, but content is not responsive.</p>
