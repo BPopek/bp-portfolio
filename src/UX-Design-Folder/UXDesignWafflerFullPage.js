e4ec80b6f913451d15e3640e77698f3f7b78eab7
Betsy Popek <betsypopek@gmail.com>
2019-12-21 14:54:16 -0700

    try 2

--- src/UX-Design-Folder/UXDesignWafflerFullPage.js
+++ src/UX-Design-Folder/UXDesignWafflerFullPage.js
@@ -1,18 +1,18 @@
-import React, {Component} from 'react';
-import WafflerFullPage from '../images/UX/WW-Portfolio.jpg';
-import { NavLink } from 'react-router-dom'
+// import React, {Component} from 'react';
+// import WafflerFullPage from '../images/UX/WW-Portfolio.jpg';
+// import { NavLink } from 'react-router-dom'
 
-class UXDesignWafflerFullPage extends Component {
+// class UXDesignWafflerFullPage extends Component {
     
-    render(){
-        return(
-            <div className='UX-FullWaffler'>
-                <NavLink to='/ux-design/waffler'>
-                    <div className='uxFullImage' background={WafflerFullPage} alt='Waffler Full Project'/>
-                </NavLink>
-            </div>
-        )
-    }
-}
+//     render(){
+//         return(
+//             <div className='UX-FullWaffler'>
+//                 <NavLink to='/ux-design/waffler'>
+//                     <div className='uxFullImage' background={WafflerFullPage} alt='Waffler Full Project'/>
+//                 </NavLink>
+//             </div>
+//         )
+//     }
+// }
 
-export default UXDesignWafflerFullPage;
+// export default UXDesignWafflerFullPage;
