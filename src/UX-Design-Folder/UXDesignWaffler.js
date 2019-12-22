218a4f9805642153cafaf39d7686158932d0421f
Betsy Popek <betsypopek@gmail.com>
2019-12-22 12:42:03 -0700

    carousel as separate component

--- src/UX-Design-Folder/UXDesignWaffler.js
+++ src/UX-Design-Folder/UXDesignWaffler.js
@@ -1,54 +1,16 @@
 import React, {Component} from 'react';
+import UXDesignWafflerCarousel from './UXDesignWafflerCarousel';
 // import Waffler from '../images/UX/WebsiteUXWafflerThumb.png';
 // import { NavLink } from 'react-router-dom'
 // import WafflerFullPage from '../images/UX/WW-Portfolio.jpg';
-import { Carousel } from 'react-responsive-carousel';
-
-import WafflerPage1 from '../images/UX_WW-Portfolio_Website_Pages-01-sm.jpg';
-import WafflerPage2 from '../images/UX_WW-Portfolio_Website_Pages-02-sm.jpg';
-import WafflerPage3 from '../images/UX_WW-Portfolio_Website_Pages-03-sm.jpg';
-import WafflerPage4 from '../images/UX_WW-Portfolio_Website_Pages-04-sm.jpg';
-import WafflerPage5 from '../images/UX_WW-Portfolio_Website_Pages-05-sm.jpg';
-import WafflerPage6 from '../images/UX_WW-Portfolio_Website_Pages-06-sm.jpg';
-import WafflerPage7 from '../images/UX_WW-Portfolio_Website_Pages-07-sm.jpg';
-import WafflerPage8 from '../images/UX_WW-Portfolio_Website_Pages-08-sm.jpg';
-import WafflerPage9 from '../images/UX_WW-Portfolio_Website_Pages-09-sm.jpg';
-
+// import { Carousel } from 'react-responsive-carousel';
 
 class UXDesignWaffler extends Component {
 
     render(){
         return(
             <div className='carouselDiv'>
-                <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} swipeable={true} dynamicHeight={true} showIndicators={true} showArrows={true} useKeyboardArrows={true}>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage1}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage2}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage3}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage4}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage5}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage6}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage7}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage8}  alt='Waffler Project'/>
-                            </div>
-                            <div className='imageCarouselUX'>
-                                <img src={WafflerPage9}  alt='Waffler Project'/>
-                            </div>
-                        </Carousel>
+                <UXDesignWafflerCarousel />
             </div>
         )
     }
---
+++ src/UX-Design-Folder/UXDesignWafflerCarousel.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+// import Waffler from '../images/UX/WebsiteUXWafflerThumb.png';
+// import { NavLink } from 'react-router-dom'
+// import WafflerFullPage from '../images/UX/WW-Portfolio.jpg';
+import { Carousel } from 'react-responsive-carousel';
+
+import WafflerPage1 from '../images/UX_WW-Portfolio_Website_Pages-01-sm.jpg';
+import WafflerPage2 from '../images/UX_WW-Portfolio_Website_Pages-02-sm.jpg';
+import WafflerPage3 from '../images/UX_WW-Portfolio_Website_Pages-03-sm.jpg';
+import WafflerPage4 from '../images/UX_WW-Portfolio_Website_Pages-04-sm.jpg';
+import WafflerPage5 from '../images/UX_WW-Portfolio_Website_Pages-05-sm.jpg';
+import WafflerPage6 from '../images/UX_WW-Portfolio_Website_Pages-06-sm.jpg';
+import WafflerPage7 from '../images/UX_WW-Portfolio_Website_Pages-07-sm.jpg';
+import WafflerPage8 from '../images/UX_WW-Portfolio_Website_Pages-08-sm.jpg';
+import WafflerPage9 from '../images/UX_WW-Portfolio_Website_Pages-09-sm.jpg';
+
+class UXDesignWafflerCarousel extends Component {
+
+    render(){
+        return(
+            
+            <Carousel showThumbs={false} infiniteLoop={true} showStatus={false} swipeable={true} dynamicHeight={true} showIndicators={true} showArrows={true} useKeyboardArrows={true}>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage1}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage2}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage3}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage4}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage5}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage6}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage7}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage8}  alt='Waffler Project'/>
+                </div>
+                <div className='imageCarouselUX'>
+                    <img src={WafflerPage9}  alt='Waffler Project'/>
+                </div>
+            </Carousel>
+        )
+    }
+}
+
+export default UXDesignWafflerCarousel;
