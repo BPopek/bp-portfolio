9cd275b40b196984adde0a279e42269823823280
Betsy Popek <betsypopek@gmail.com>
2019-12-22 11:03:52 -0700

    trying images folder

--- src/UX-Design-Folder/UXDesignWaffler.js
+++ src/UX-Design-Folder/UXDesignWaffler.js
@@ -4,15 +4,15 @@ import React, {Component} from 'react';
 // import WafflerFullPage from '../images/UX/WW-Portfolio.jpg';
 import { Carousel } from 'react-responsive-carousel';
 
-import WafflerPage1 from '../images/UX/WW-Portfolio_Website_Pages-01-sm.jpg';
-import WafflerPage2 from '../images/UX/WW-Portfolio_Website_Pages-02-sm.jpg';
-import WafflerPage3 from '../images/UX/WW-Portfolio_Website_Pages-03-sm.jpg';
-import WafflerPage4 from '../images/UX/WW-Portfolio_Website_Pages-04-sm.jpg';
-import WafflerPage5 from '../images/UX/WW-Portfolio_Website_Pages-05-sm.jpg';
-import WafflerPage6 from '../images/UX/WW-Portfolio_Website_Pages-06-sm.jpg';
-import WafflerPage7 from '../images/UX/WW-Portfolio_Website_Pages-07-sm.jpg';
-import WafflerPage8 from '../images/UX/WW-Portfolio_Website_Pages-08-sm.jpg';
-import WafflerPage9 from '../images/UX/WW-Portfolio_Website_Pages-09-sm.jpg';
+import WafflerPage1 from '../images/UX_WW-Portfolio_Website_Pages-01-sm.jpg';
+import WafflerPage2 from '../images/UX_WW-Portfolio_Website_Pages-02-sm.jpg';
+import WafflerPage3 from '../images/UX_WW-Portfolio_Website_Pages-03-sm.jpg';
+import WafflerPage4 from '../images/UX_WW-Portfolio_Website_Pages-04-sm.jpg';
+import WafflerPage5 from '../images/UX_WW-Portfolio_Website_Pages-05-sm.jpg';
+import WafflerPage6 from '../images/UX_WW-Portfolio_Website_Pages-06-sm.jpg';
+import WafflerPage7 from '../images/UX_WW-Portfolio_Website_Pages-07-sm.jpg';
+import WafflerPage8 from '../images/UX_WW-Portfolio_Website_Pages-08-sm.jpg';
+import WafflerPage9 from '../images/UX_WW-Portfolio_Website_Pages-09-sm.jpg';
 
 
 class UXDesignWaffler extends Component {
