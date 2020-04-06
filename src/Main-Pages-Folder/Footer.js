72b2f8fb8f795557061b7e7c06717aa2c4dcee94
Betsy Popek <betsypopek@gmail.com>
2020-04-06 15:17:14 -0600

    fixed navbar for small screens, links

--- src/Main-Pages-Folder/Footer.js
+++ src/Main-Pages-Folder/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Footer() {
     return(
         <div className='footCont'>
-            <a href='https://www.linkedin.com/in/betsypopek/' className='footerLink'>LinkedIn</a>
-            <a href='https://github.com/BPopek' className='footerLink'>GitHub</a>
-            <a href='https://www.instagram.com/betsypopek/' className='footerLink'>Instagram</a>
-            <a href="mailto:betsypopek@gmail.com" className='footerLink'>Say Hello!</a>
+            <a href='https://www.linkedin.com/in/betsypopek/' className='footerLink' target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href='https://github.com/BPopek' className='footerLink' target="_blank" rel="noopener noreferrer">GitHub</a>
+            <a href='https://www.instagram.com/betsypopek/' className='footerLink' target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="mailto:betsypopek@gmail.com" className='footerLink' target="_blank" rel="noopener noreferrer">Say Hello!</a>
         </div>
     )
 }
--- src/Main-Pages-Folder/Navbar.js
+++ src/Main-Pages-Folder/Navbar.js
@@ -7,9 +7,12 @@ class Navbar extends Component {
         return(
             <div className='navbarCont'>
                 <NavLink exact to='/' className='navbarLink' activeClassName='navbarLinkActive'>Home</NavLink>
-                <NavLink exact to='/web-design' className='navbarLink' activeClassName='navbarLinkActive'>Web Design</NavLink>
-                <NavLink exact to='/graphic-design' className='navbarLink' activeClassName='navbarLinkActive'>Graphic Design</NavLink>
-                <NavLink exact to='/ux-design' className='navbarLink' activeClassName='navbarLinkActive'>UX Design</NavLink>
+                <NavLink exact to='/web-design' className='navbarLinkLg' activeClassName='navbarLinkActive'>Web Design</NavLink>
+                <NavLink exact to='/graphic-design' className='navbarLinkLg' activeClassName='navbarLinkActive'>Graphic Design</NavLink>
+                <NavLink exact to='/ux-design' className='navbarLinkLg' activeClassName='navbarLinkActive'>UX Design</NavLink>
+                <NavLink exact to='/web-design' className='navbarLinkSm' activeClassName='navbarLinkActive'>Web</NavLink>
+                <NavLink exact to='/graphic-design' className='navbarLinkSm' activeClassName='navbarLinkActive'>Graphic</NavLink>
+                <NavLink exact to='/ux-design' className='navbarLinkSm' activeClassName='navbarLinkActive'>UX</NavLink>
                 <NavLink exact to='/profile' className='navbarLink' activeClassName='navbarLinkActive'>Profile</NavLink>
             </div>
         )
