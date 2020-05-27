980538d6956bbe9296c8fce7cee69de91056e5ce
BPopek <betsypopek@gmail.com>
2020-05-27 16:04:49 -0600

    updated resume

--- src/Main-Pages-Folder/Profile.js
+++ src/Main-Pages-Folder/Profile.js
@@ -10,14 +10,15 @@ function Profile () {
         <div className='profile'>
             <h3 className='welcome'>Hello! I'm Betsy, a Salt Lake City, UT based Software Developer.</h3>
             <h4 className='careerCode'>career = graphicDesigner => softwareDeveloper</h4>
+            <a download href={BPopekResume}  className='resumeLink'>DOWNLOAD RESUME</a>
             <p className='profileHistory'>My interest in coding grew over the years while I worked as a graphic designer. Since beginning my design journey over a decade ago, I've dabbled in courses for CSS, HTML, Ruby and Python as well as Adobe Dreamweaver. After completing a UX online course through Springboard, I realized I loved the user experience but wanted to be more involved in coding and building interfaces instead of handing that off to a developer. I finally decided to take the plunge and attend the VSchool Full-Stack (MERN) JavaScript Bootcamp in downtown SLC this year because I was drawn to their package of a wealth of consistently updated material with a fabulous team and a high level of alumni support. As a software engineer I value building clean designs with an eye for thoughtful user experience.</p>
             </div>
         <div className='profile2'>
             <h5 className='profileHeader'>Software Development:</h5>
             <hr className='profileLineSD' />
-            <p className='profileSmHead'>Front End: <span className='profileDetail'>React, JavaScript, ES6, HTML5, CSS, Sass</span></p> 
+            <p className='profileSmHead'>Front End: <span className='profileDetail'>React, JavaScript, ES6, HTML5, CSS, Sass, Bootstrap, jQuery</span></p> 
             <p className='profileSmHead'>Back End: <span className='profileDetail'>Node.js, Express, MongoDB, Mongoose</span></p>
-            <p className='profileSmHead'>Other: <span className='profileDetail'>Git, JSON</span></p>
+            <p className='profileSmHead'>Other: <span className='profileDetail'>Git, JSON, Agile, Wordpress</span></p>
             <h5 className='profileHeader'>Design:</h5>
             <hr className='profileLineD' />
             <p className='profileSmHead'>Adobe Creative Suite: <span className='profileDetail'>InDesign, Photoshop, Illustrator</span></p>
@@ -28,7 +29,6 @@ function Profile () {
             {/* <p className='profileSmHead'>Email: <span className='profileDetail'>betsypopek@gmail.com</span></p> */}
             <p className='profileSmHead'>Email: <a href="mailto:betsypopek@gmail.com" className='profileDetailEmail'>betsypopek@gmail.com</a></p>
             <p className='profileSmHeadPhone'>Phone: <span className='profileDetail'>801-448-7273</span></p>
-            <a download href={BPopekResume}  className='resumeLink'>DOWNLOAD RESUME</a>
         </div>
         </>
     )
